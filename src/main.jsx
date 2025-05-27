9dae5f4fbc2c5970cb6bbafdffe714756c24013a
BaptisteTP <150006584+BaptisteTP@users.noreply.github.com>
2025-05-27 23:20:45 +0200

    rootage index html

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 // main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
+import Home from './pages/Home.jsx'; // Importez le composant Home
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<Home />} /> {/* Route vers Home */}
+      <Route path="*" element={<App />} /> {/* Route par défaut */}
+    </Routes>
   </BrowserRouter>
 );
